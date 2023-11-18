2727f7a45cc1607afd78019232aa980d29692cab
irinadjokic <ikidjokic@gmail.com>
2023-11-18 13:39:35 +0100

    radii

---
+++ 01-simple/Models2/Actor.cs
@@ -0,0 +1,11 @@
+namespace DSS.Models
+{
+    public class Actor: Man
+    {
+        public duble ImdbMark = 5 ;
+
+        public int NumberOfOscars  = 0 ;
+
+        public int NumberOfBaftas = 0;
+    }
+}
---
+++ 01-simple/Models2/PostalAddress.cs
@@ -0,0 +1,14 @@
+namespace DSS.Models
+{
+    public class PostalAddress
+    {
+        public string Streat = "Unknown";
+
+        public string City ="Unknown";
+
+        public string PostalCode ="Unknown";
+
+        public string State = "Unknown";
+
+    }
+}
